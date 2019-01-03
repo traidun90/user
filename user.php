6e90a823878dba4845d44168815208685cb02c7f
traidun09 <traidun90@mail.ru>
2019-01-03 17:12:41 +0200

    Update user.php

--- user.php
+++ user.php
@@ -17,7 +17,7 @@ $msgveeu = "[!] enjoy your life";
 $data= ['adi bordir','adi bordir',];
 
 //bot veeu
-$ver_veeu = ['5.03.214',];
+$ver_veeu = ['5.03.215',];
 $user_veeu= ['irina toder','iulik galias',];
 
 $toto = ['tes',];
